eae284eaa807352f58f88cfa76c88a1db2bb9890
Mukunda Modell <mmodell@wikimedia.org>
2020-04-15 18:34:53 -0500

    Add stubs for Turkish and Ukrainian translations.

---
+++ src/locales/TranslateWikiUkrainianUk.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * TranslateWiki Locale for Ukrainian localization in Phabricator
+ */
+final class TranslateWikiUkrainianUk extends PhutilLocale {
+    public function getLocaleCode() {
+        return 'uk';
+    }
+
+    public function getLocaleName() {
+        return pht('Ukrainian');
+    }
+}
