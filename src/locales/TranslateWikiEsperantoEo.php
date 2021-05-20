f1d620f79db874165e006102b4b330fe87c2c584
Mukunda Modell <mmodell@wikimedia.org>
2021-05-20 13:33:06 -0500

    Add esperanto locale
    fixes T283105
    
    Bug: T283105

---
+++ src/locales/TranslateWikiEsperantoEo.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * TranslateWiki Locale for Greek (Greece) localization in Phabricator
+ */
+final class TranslateWikiEsperantoEo extends PhutilLocale {
+    public function getLocaleCode() {
+        return 'eo';
+    }
+
+    public function getLocaleName() {
+        return pht('Esperanto');
+    }
+}
