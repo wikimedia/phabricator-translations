cc2bd68a1355d9814bb270dc682e76aae4c59a9c
Pppery <mapreader@olum.org>
2025-08-31 20:32:16 -0400

    Remove fallback for Asturian language
    Bug: T292750
    Change-Id: I90ec580b033a2b69889e9186188d165f1b5b649a

--- src/locales/TranslateWikiAsturianAst.php
+++ src/locales/TranslateWikiAsturianAst.php
@@ -11,8 +11,5 @@ final class TranslateWikiAsturianAst extends PhutilLocale {
   public function getLocaleName() {
     return pht('Asturian');
   }
-  public function getFallbackLocaleCode() {
-    return 'es_ES';
-  }
 }
 
